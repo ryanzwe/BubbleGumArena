28c8f790905a4ff11792c28d56d9be6ec999a330
LucasClark <37648074+LucasClark@users.noreply.github.com>
2018-03-29 23:19:42 +1000

    Add files via upload

---
+++ Assets/Scripts/Player_Attack.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Attack : MonoBehaviour {
+
+
+    [SerializeField] string attackButton;
+    [SerializeField] bool attack;
+    [SerializeField] float speed;
+    [SerializeField] float knockUp;
+    Animator anim;
+    Rigidbody otherBody;
+    void Start ()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            otherBody = collision.gameObject.GetComponent<Rigidbody>();
+            otherBody.AddForce((gameObject.transform.forward - (gameObject.transform.up/knockUp)) * speed);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetButton(attackButton))
+            anim.SetBool("Attack", true);
+        else
+            anim.SetBool("Attack", false);
+    }
+}
---
+++ Assets/Scripts/Test_Object_Drop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Object_Drop : MonoBehaviour {
+
+    [SerializeField] string attackButton;
+    [SerializeField] GameObject myBox;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown(attackButton))
+        {
+            Instantiate(myBox);
+        }
+
+    }
+}
