0db418d2731317b0d79378ed6959d874be49337a
Rartwo <37647027+Rartwo@users.noreply.github.com>
2018-04-27 08:47:27 +1000

    EnvironmentChange
    Environment;
    
    Updated gumball environment
    Renamed Gumball arena level
    Created a bunch of materials (generic colors)

---
+++ Assets/rotatorCam.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotatorCam : MonoBehaviour {
+        float yRot = 0.1f;
+	// Use this for initialization
+	void Start () {
+
+        
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(0, yRot, 0);
+
+	}
+}
