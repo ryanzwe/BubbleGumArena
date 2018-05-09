fed90908740d0707fbea23bf01cb39aadcb5d2ee
Ryan Wilton <ryanwilton@live.com.au>
2018-05-09 22:40:48 +1000

    Basic level selector framework

---
+++ Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+    private int levelIndex = 1;
+    [SerializeField]
+    private Sprite[] levelImages;
+    [SerializeField]
+    private Sprite currentLevelSprite;
+
+
+    public void LoadLevelIndex()
+    {
+        SceneManager.LoadScene("Level" + levelIndex);
+    }
+
+    public void LevelIndexModifier(int amount)
+    {
+        levelIndex += amount;
+        if (levelIndex > levelImages.Length)
+            levelIndex = 1;
+        else if (levelIndex < levelImages.Length)
+            levelIndex = levelImages.Length - 1;
+
+        currentLevelSprite = levelImages[levelIndex];
+    }
+}
