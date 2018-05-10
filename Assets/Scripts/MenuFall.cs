eebe60110fadb33990a90f7d2b1025df81e283be
Rartwo <37647027+Rartwo@users.noreply.github.com>
2018-05-11 01:13:50 +1000

    UI fixed and updated...
    Except Quit Button on main menu

---
+++ Assets/Scripts/MenuFall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuFall : MonoBehaviour
+{
+
+
+    [SerializeField]
+    List<Transform> playerObjects;
+    [SerializeField] float respawnHeight;
+    [SerializeField]
+    float offsetReSpawn;
+    // Use this for initialization 
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		foreach(Transform yay in playerObjects)
+        {
+            if(yay.position.y <= respawnHeight)
+            {
+                yay.position += new Vector3(0, offsetReSpawn, 0);
+            }
+        }
+	}
+}
